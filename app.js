7e30097c4d0414874180f8f80f30a9eac24898b2
unknown <do.potolka.korona@gmail.com>
2021-04-10 11:00:35 +0500

    ser

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 
 app.use(express.json({ extended: true }))
-
+app.get('/', (req, res) => res.send('API Running'))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/table', require('./routes/table.routes'))
 
