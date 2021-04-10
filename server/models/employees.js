ed179a1fc3e3ef94fb44fe93093da4871a16c20d
unknown <do.potolka.korona@gmail.com>
2021-04-10 10:03:27 +0500

    jkh

--- server/models/employees.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model, Types } = require('mongoose')
-
-const schema = new Schema({
-  tabNumber: { type: Number, required: true, unique: true },
-  isAdmin: { type: Boolean, required: true },
-})
-
-module.exports = model('Employees', schema)
